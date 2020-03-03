8943a1c52fbd1f06b51ad06a1c0e24c47a19a38b
VSCAdmin <VSCAdmin@192.168.100.94>
2020-03-03 11:21:12 +0530

    this is my first commit

---
+++ src/main/java/com/git/practice/GitPractice.java
@@ -0,0 +1,15 @@
+package com.git.practice;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		m1();
+
+	}
+
+	private static void m1() {
+		System.out.println("iam from m1");
+		
+	}
+
+}
